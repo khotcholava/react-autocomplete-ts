48d9c689e2abe8033c07213533e201ed2b083b93
khotcholava <akokhocholava@gmail.com>
2022-01-03 21:25:25 +0400

    move types into separate file

---
+++ src/Components/Autocomplete/Autocomplete.types.ts
@@ -0,0 +1,20 @@
+export interface AutocompleteProps<T> {
+  options: Array<Option<T>>;
+  placeholder: string;
+  value: T | null;
+  onChange: (value: T | null) => any,
+  bindKey: (value: T) => string | number;
+  onSearchChange?: (input: string) => void
+}
+
+export interface Option<T> {
+  label: string;
+  value: T;
+}
+
+
+export enum KEYBOARD_KEYS  {
+  ENTER=  'Enter',
+  ARROW_UP =  "ArrowUp",
+  ARROW_DOWN = "ArrowDown"
+}
